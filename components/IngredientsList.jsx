c9af481dbb394026b6b6144ab25fddc5c50f995c
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-04-29 23:43:57 -0400

    integrated AI

--- components/IngredientsList.jsx
+++ components/IngredientsList.jsx
@@ -1,5 +1,23 @@
-export default function IngredientsList({items}){
+export default function IngredientsList({getRecipe, ingredients}){
+    const listItems = ingredients.map(item => (
+      <li key={item}>{item}</li> 
+     
+    ))
+  
   return(
-    <li>{items}</li>
+    <section>
+    <h2>Ingredients on hand:</h2>
+    <ul className="list-items">
+      {listItems}
+    </ul>
+    {ingredients.length > 3 && <div className="recipe">
+      <div>
+        <h3>Ready for a recipe?</h3>
+        <p>Generate a recipe from your list of ingredients.</p>
+      </div>
+      <button onClick={getRecipe}>Get a recipe</button>
+    </div>}
+  </section>
+    
   )
 }
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,17 +1,15 @@
 import { useState } from "react"
 import IngredientsList from "./IngredientsList"
 import ClaudeRecipe from "./ClaudeRecipe"
+import { getRecipeFromMistral } from "../ai"
 
 
 export default function Main() {
   
 
   const [ingredients, setIngredients] = useState([])
-  const listItems = ingredients.map(item => (
-    <li key={item}>{item}</li> 
-   
-  ))
-  const [recipeShown, setRecipeShown] = useState(false)
+
+  const [recipe, setRecipe] = useState('')
 
   function addIngredient(formData) {
     const newIngredient = formData.get('item')
@@ -19,8 +17,9 @@ export default function Main() {
 
   }
 
-  function showRecipe() {
-    setRecipeShown(prevRecipe => !prevRecipe)
+  async function getRecipe() {
+    const recipeMarkdown = await getRecipeFromMistral(ingredients)
+    setRecipe(recipeMarkdown)
   }
 
   return (
@@ -32,22 +31,9 @@ export default function Main() {
         <button type="submit">Add an ingredient</button>
       </form>
       <p className="info">You need to add at least 4 ingredients.</p>
-      {ingredients.length > 0 &&
-        <section>
-          <h2>Ingredients on hand:</h2>
-          <ul className="list-items">
-            {listItems}
-          </ul>
-          {ingredients.length > 3 && <div className="recipe">
-            <div>
-              <h3>Ready for a recipe?</h3>
-              <p>Generate a recipe from your list of ingredients.</p>
-            </div>
-            <button onClick={showRecipe}>Get a recipe</button>
-          </div>}
-        </section>}
+      {ingredients.length > 0 && <IngredientsList getRecipe = {getRecipe} ingredients ={ingredients}/>}
 
-      {recipeShown && <ClaudeRecipe />}
+      {recipe && <ClaudeRecipe recipe={recipe} />}
     </main>
   )
 }
